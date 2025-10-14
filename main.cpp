fc312a97d3cdd2f2090705faafbd57bdcae91286
Bartek21371 <bartolino2005@gmail.com>
2025-10-15 00:10:44 +0200

    First commit setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include "include/calculator.hpp"
 #include <iostream>
-
-
+#include <string>
 
 int main() {
-    return 0;
+    std::string p = "";
+    std::cout<<"Hello in calculator with parser!"<<std::endl;
+    std::cout<<"Enter your operation to calculate: "<<std::endl;
+    std::getline(std::cin, p);;
 }
