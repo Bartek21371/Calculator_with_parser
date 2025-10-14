e060cf3e357463a990c2ee448468efab3b7a5ac7
Bartek21371 <bartolino2005@gmail.com>
2025-10-15 00:12:37 +0200

    Second commit setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "include/calculator.hpp"
 #include <iostream>
 #include <string>
-
+//only read value and check is good
 int main() {
     std::string p = "";
     std::cout<<"Hello in calculator with parser!"<<std::endl;
